92aa30420a0674eefc89fd9c0844cefba7f78de1
Abdullah Böğrek <49994631+AbdullahBogrek@users.noreply.github.com>
2022-11-09 14:04:46 +0300

    Update PageNotFound

--- frontend/src/views/PageNotFound.vue
+++ frontend/src/views/PageNotFound.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
+    <div class="container shadow">
         <div class="row d-flex justify-content-center">
-            <img src="../assets/page_not_found.png" alt="404 Page Not Found" class="page-not-found shadow">
-            <h5 class="text-center mt-5">Aradığınız sayfa bulunamadı. Adresinizi kontrol ederek tekrar deneyiniz. İsterseniz <RouterLink to="/">buradan</RouterLink> anasayfaya gidebilirsiniz.</h5>
+            <img src="../assets/page_not_found.png" alt="404 Page Not Found" class="page-not-found">
+            <h5 class="text-center my-5">Aradığınız sayfa bulunamadı. Adresinizi kontrol ederek tekrar deneyiniz. İsterseniz <RouterLink to="/">buradan</RouterLink> anasayfaya gidebilirsiniz.</h5>
         </div>
     </div>
 </template>
